2046204de190c35c0846580178d4ef1efc465163
MichaelLearnCode <michael.nuyennuyen@gmail.com>
2025-03-20 20:54:07 +0700

    position

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,16 @@ if (navbarLists && navbarToggler){
         navbarLists.classList.toggle('collasped');
     })
 }
+
+const navLinks = document.querySelectorAll('.nav-link-item');
+
+navLinks.forEach(navLink=>{
+    if (navLink){
+        navLink.addEventListener('click', ()=>{
+            const dropDown = navLink.querySelector('.dropdown-menu');
+            if (dropDown){
+                dropDown.classList.toggle('open');
+            }
+        })
+    }
+})
