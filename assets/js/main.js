0cb4bedc022972df2b92aa8aaf8045c7bf014d62
MichaelLearnCode <michael.nuyennuyen@gmail.com>
2025-03-24 22:36:37 +0700

    modal added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
 const navbarLists = document.querySelector('.navbar .nav-link-lists');
 const navbarToggler = document.querySelector('.navbar .navbar-toggler');
 
@@ -17,5 +20,35 @@ navLinks.forEach(navLink=>{
                 dropDown.classList.toggle('open');
             }
         })
+    }
+})
+
+// 
+
+const modalTriggers = document.querySelectorAll('*[data-toggle = "modal"]');
+modalTriggers.forEach(modalTrigger=>{
+    if (!modalTrigger)return;
+    modalTrigger.onclick = function(e){
+        e.preventDefault();
+        const modal = document.getElementById(this.getAttribute('data-target'));
+        if (!modal)return;
+        modal.classList.add('show');
+        const modalTitle = modal.querySelector('.modal-title');
+        if (modalTitle)
+        modalTitle.textContent = this.getAttribute('data-title') ?? 'default title';
+        const modalBody = modal.querySelector('.modal-body');
+        if (modalBody)modalBody.textContent = this.getAttribute('data-body') ?? 'default body';
+    }
+})
+
+const closeBtns = document.querySelectorAll('*[data-dismiss]');
+closeBtns.forEach(closeBtn=>{
+    if (!closeBtn)return;
+    closeBtn.onclick = function(e){
+        if (this.getAttribute('data-dismiss')==='modal'){
+            const modal = this.closest('.modal');
+            if (!modal)return;
+            modal.classList.remove('show');
+        }
     }
 })
